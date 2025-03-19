b2c3170b5c1aef1935c72fe591ed318fad102863
Bastaks14096 <taksin.k@kkumail.com>
2025-03-19 14:21:27 +0700

    add card

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,10 @@
 <script lang="ts">
-export const description
-  = 'A sidebar that collapses to icons.'
-export const iframeHeight = '800px'
-export const containerClass = 'w-full h-full'
+export const description = "A sidebar that collapses to icons.";
+export const iframeHeight = "800px";
+export const containerClass = "w-full h-full";
 </script>
 
 <script setup lang="ts">
-
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -14,20 +12,22 @@ import {
   BreadcrumbList,
   BreadcrumbPage,
   BreadcrumbSeparator,
-} from '@/components/ui/breadcrumb'
-import { Separator } from '@/components/ui/separator'
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
-} from '@/components/ui/sidebar'
+} from "@/components/ui/sidebar";
 </script>
 
 <template>
   <SidebarProvider>
     <AppSidebar />
     <SidebarInset>
-      <header class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+      <header
+        class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12"
+      >
         <div class="flex items-center gap-2 px-4">
           <SidebarTrigger class="-ml-1" />
           <Separator orientation="vertical" class="mr-2 h-4" />
@@ -46,7 +46,9 @@ import {
           </Breadcrumb>
         </div>
       </header>
-      <div class="flex flex-1 flex-col gap-4 p-4 pt-0">
+      <div
+        class="max-w-[1980px] w-full m-auto flex flex-1 flex-col gap-4 p-4 pt-0"
+      >
         <slot />
       </div>
     </SidebarInset>
