92b7256bbde0a9e173c449a7c7347a3b57a84d26
MSI\prame <prametanaset147@gmail.com>
2025-03-19 10:59:45 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Button>Click me</Button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Button } from '@/components/ui/button'
+</script>
+
+<style>
+
+</style>
