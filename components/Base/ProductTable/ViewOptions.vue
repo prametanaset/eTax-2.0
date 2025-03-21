f940e1c19dcd7890037a4085b8a674e8c77c2d69
Bastaks14096 <taksin.k@kkumail.com>
2025-03-21 16:56:16 +0700

    add page product

--- components/Base/ProductTable/ViewOptions.vue
+++ components/Base/ProductTable/ViewOptions.vue
@@ -1,5 +1,49 @@
 <template>
   <DropdownMenu>
+    <div class='mr-4'>
+        <BaseDialog text='เพิ่มสินค้า' bg-button='bg-primary-500' title='เพิ่มสินค้า' >
+            <template #content>
+                <div class='grid gap-y-4'>
+                  <div>
+                    <Label>ชื่อสินค้า</Label>
+                    <Input placeholder='กรอกชื่อสินค้า' v-model='product.name'/>
+                  </div>
+                  <div>
+                    <Label>รายละเอียดสินค้า</Label>
+                    <Textarea placeholder='กรอกชื่อสินค้า' v-model='product.desc'/>
+                  </div>
+                  <div>
+                    <Label>ราคา</Label>
+                    <Input placeholder='0' type='number' v-model='product.price'/>
+                  </div>
+                  <div class='grid gap-2'>
+                    <div class='flex gap-2 items-center'>
+                      <Switch id="airplane-mode" v-model='product.include_vat'/>
+                      <Label>VAT %</Label>
+                    </div>
+                    <Input v-if='product.include_vat' placeholder='0' type='number' min='0' v-model='product.vat_value'/>
+                    <!-- <NumberField  v-if='product.include_vat' v-model='product.vat_value' >
+                      <NumberFieldContent>
+                        <NumberFieldDecrement />
+                        <NumberFieldInput />
+                        <NumberFieldIncrement />
+                      </NumberFieldContent>
+                    </NumberField> -->
+                  </div>
+                </div>
+            </template> 
+            <template #footer>
+              <div class='flex justify-end '>
+                <div class='flex gap-2'>
+                  <DialogClose as-child>
+                    <Button variant="outline">ยกเลิก</Button>
+                  </DialogClose>
+                  <Button type="submit">สร้าง</Button>
+                </div>
+              </div>
+            </template>
+          </BaseDialog>
+      </div>
     <DropdownMenuTrigger as-child>
       <Button
         variant="outline"
@@ -31,6 +75,7 @@
 import type { Table } from '@tanstack/vue-table'
 import type { Product } from './data/schema'
 import { Button } from '@/components/ui/button'
+import { Switch } from '@/components/ui/switch'
 import {
   DropdownMenu,
   DropdownMenuCheckboxItem,
@@ -41,6 +86,13 @@ import {
 } from '@/components/ui/dropdown-menu'
 
 import MixerHorizontalIcon from '~icons/radix-icons/mixer-horizontal'
+import {
+  NumberField,
+  NumberFieldContent,
+  NumberFieldDecrement,
+  NumberFieldIncrement,
+  NumberFieldInput,
+} from '@/components/ui/number-field'
 
 interface DataTableViewOptionsProps {
   table: Table<Product>
@@ -53,6 +105,16 @@ const columns = computed(() => props.table.getAllColumns()
     column =>
       typeof column.accessorFn !== 'undefined' && column.getCanHide(),
   ))
+
+
+const product = reactive({
+  id: 0,
+  name: '',
+  desc: '',
+  price: 0,
+  include_vat: false,
+  vat_value: 0
+})
 </script>
 
 <style>
--- pages/category/index.vue
+++ pages/category/index.vue
@@ -1,48 +1,15 @@
 <template>
   <div>
-    <Card>
-      <div class="flex justify-between p-4 gap-4">
-        <div class="relative w-full max-w-sm items-center">
-          <Input id="search" type="text" placeholder="ค้นหาสินค้า..." class="pl-10" />
-          <span class="absolute start-0 inset-y-0 flex items-center justify-center px-2">
-            <Search class="size-6 text-muted-foreground" />
-          </span>
-        </div>
-        <div class="">
-          <BaseDialog text='เพิ่มสินค้า' bg-button='bg-primary-500' title='เพิ่มสินค้า' >
-            <template #content>
-                <div class='grid gap-y-4'>
-                  <div>
-                    <Label>ชื่อสินค้า</Label>
-                    <Input placeholder='กรอกชื่อสินค้า'/>
-                  </div>
-                  <div>
-                    <Label>รายละเอียดสินค้า</Label>
-                    <Textarea placeholder='กรอกชื่อสินค้า'/>
-                  </div>
-                  <div>
-                    <Label>ราคา</Label>
-                    <Input placeholder='0' type='number'/>
-                  </div>
-                </div>
-            </template>
-            <template #footer>
-              <div class='flex justify-end '>
-                <div class='flex gap-2'>
-                  <Button variant="outline">ยกเลิก</Button>
-                  <Button type="submit">สร้าง</Button>
-                </div>
-              </div>
-            </template>
-          </BaseDialog>
-        </div>
+      <div>
+        <BaseProductTable :data="products_data" :columns="columns"></BaseProductTable>
       </div>
-    </Card>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { Search } from 'lucide-vue-next'
+import products_data from '@/public/data/products_100_realistic.json'
+import { columns } from '@/components/Base/ProductTable/columns'
 
 definePageMeta({
   title:'รายการสินค้า'
