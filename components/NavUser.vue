8854311d6bf2c667fc6bb876080cd42b023b59a6
prametanaset <prametanaset147@gmail.com>
2025-07-23 02:48:12 +0700

    refactor: enhance styling of dropdown menu and avatar components for consistency

--- components/NavUser.vue
+++ components/NavUser.vue
@@ -43,7 +43,7 @@ const { isMobile } = useSidebar();
         <DropdownMenuTrigger as-child>
           <SidebarMenuButton
             size="lg"
-            class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+            class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground rounded-xl"
           >
             <Avatar class="h-8 w-8 rounded-full">
               <AvatarImage :src="user.avatar" :alt="user.name" />
@@ -57,8 +57,8 @@ const { isMobile } = useSidebar();
           </SidebarMenuButton>
         </DropdownMenuTrigger>
         <DropdownMenuContent
-          class="w-[--reka-dropdown-menu-trigger-width] min-w-56 rounded-lg"
-          :side="isMobile ? 'bottom' : 'right'"
+          class="w-[--reka-dropdown-menu-trigger-width] ml-2 mb-1 min-w-56 rounded-xl"
+          :side="isMobile ? 'bottom' : 'bottom'"
           align="end"
           :side-offset="4"
         >
@@ -66,7 +66,7 @@ const { isMobile } = useSidebar();
             <div class="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
               <Avatar class="h-8 w-8 rounded-full">
                 <AvatarImage :src="user.avatar" :alt="user.name" />
-                <AvatarFallback class="rounded-lg"> CN </AvatarFallback>
+                <AvatarFallback class="rounded-xl"> CN </AvatarFallback>
               </Avatar>
               <div class="grid flex-1 text-left text-sm leading-tight">
                 <span class="truncate font-semibold">{{ user.name }}</span>
