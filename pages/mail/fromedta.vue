54fd870b2bb37dab313b1b03a2e828332ffc3b1e
Bastaks14096 <taksin.k@kkumail.com>
2025-07-23 16:34:06 +0700

    mail display animation smooth

--- pages/mail/fromedta.vue
+++ pages/mail/fromedta.vue
@@ -5,12 +5,9 @@
       <BaseMailNotionMailList />
     </div>
     <div
-    v-if="mailStore.selectMail?.data.html"
       :class="[
-        'absolute z-[99] bg-background shadow-lg w-full xl:w-[690px] h-full xl:border border-muted-300/20 dark:border-muted-800 transition-transform duration-200 ease-in-out top-0 overflow-y-auto',
-        mailStore.selectMail !== null
-          ? 'translate-x-0 xl:right-0'
-          : 'translate-x-full xl:right-[-690px]',
+        'absolute z-[99] bg-background shadow-lg w-full xl:w-[690px] h-full xl:border border-muted-300/20 dark:border-muted-800 transition-transform duration-300 ease-in-out top-0 right-0 overflow-y-auto',
+        mailStore.selectMail !== null ? 'translate-x-0' : 'translate-x-full',
       ]"
     >
       <BaseMailNotionMailDisplay />
